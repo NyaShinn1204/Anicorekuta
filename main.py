b15ba215cbc9ea0be150d188a89cbca75b09a0c0
NyaShinn1204 <team.nyarara@gmail.com>
2024-08-26 16:42:08 +0900

    Add Icons
    - アイコンの追加

--- main.py
+++ main.py
@@ -24,12 +24,39 @@ root.title("Hotaru-WV | v0.0.1")
 root.geometry("1366x768")
 root.resizable(0, 0)
 
+def clear_frame(frame):
+  for widget in frame.winfo_children():
+    widget.destroy()
+  frame.pack_forget()
+
+def setup_cotent_sidebar(num1, num2, num3):
+  global module_frame
+  frame_scroll = module_frame = ctk.CTkScrollableFrame(root, fg_color="#0f1314", bg_color="#152945", width=1000, height=630)
+  module_frame.place(x=245, y=70)
+  clear_frame(frame_scroll)
+  if num1 == 1:
+    if num2 == 1:
+      if num3 == 1:
+        printl("info", "Open Abema Downloader")
+      if num3 == 2:
+        printl("info", "Open U-next Downloader")
+      if num3 == None:
+        printl("info", "Open All DOwnloader")
+    if num2 == 2:
+      if num3 == 1:
+        printl("info", "Open Download List")
+      if num3 == 2:
+        printl("info", "Open Already Download List")
+  if num1 == 2:
+    if num2 == 1:
+      printl("info", "Open About Tab")
+
 def setup_sidebar():
   # 画像をRGBAモードで開く
   original_image = Image.open("./data/icon.png").convert("RGBA")
   
   # 画像のサイズを指定
-  size = (65, 65)
+  size = (50, 50)
   
   # 画像をリサイズ
   image_resized = original_image.resize(size, Image.LANCZOS)
@@ -56,12 +83,51 @@ def setup_sidebar():
   tk.Label(root, bg="#3f5673", width=1024, height=720).place(x=0,y=0)
   tk.Label(root, bg="#688fbf", width=32, height=720).place(x=0,y=0)
   
-  ctk.CTkLabel(master=root,image=logo_image,text="").place(x=5,y=5)
-  tk.Label(root, bg="#688fbf", text="Hotaru-WV", fg="#fff", font=("Dubai Medium", 13, "normal")).place(x=85,y=0)
-  tk.Label(root, bg="#688fbf", text=version, fg="#505b5e", font=("Dubai Medium", 13, "normal")).place(x=85,y=25)
+  ctk.CTkLabel(master=root,image=logo_image,text="").place(x=15,y=5)
+  tk.Label(root, bg="#688fbf", text="Hotaru-WV", fg="#fff", font=("Dubai Medium", 13, "normal")).place(x=70,y=0)
+  tk.Label(root, bg="#688fbf", text=version, fg="#505b5e", font=("Dubai Medium", 13, "normal")).place(x=70,y=25)
   
   modulelist = ctk.CTkFrame(master=root, width=230, height=720, corner_radius=0, fg_color="#688fbf")
   modulelist.place(x=0,y=100)
+  
+  
+  def on_enter_home(event):
+    button_home.configure(image=ctk.CTkImage(Image.open("data/icon_home_activate.png"),size=(20, 20)))
+  
+  def on_leave_home(event):
+    button_home.configure(image=ctk.CTkImage(Image.open("data/icon_home_disable.png"),size=(20, 20)))
+  
+  def on_enter_library(event):
+    button_library.configure(image=ctk.CTkImage(Image.open("data/icon_library_activate.png"),size=(20, 20)))
+  
+  def on_leave_library(event):
+    button_library.configure(image=ctk.CTkImage(Image.open("data/icon_library_disable.png"),size=(20, 20)))
+    
+  def on_enter_library_already(event):
+    button_library_already.configure(image=ctk.CTkImage(Image.open("data/icon_library_activate.png"),size=(20, 20)))
+  
+  def on_leave_library_already(event):
+    button_library_already.configure(image=ctk.CTkImage(Image.open("data/icon_library_disable.png"),size=(20, 20)))
+  
+  button_home = ctk.CTkButton(master=modulelist, command=lambda: setup_cotent_sidebar(1, 1, None), image=ctk.CTkImage(Image.open("data/icon_home_disable.png"),size=(20, 20)), compound="left", fg_color="#0f1314", hover_color="#2b373a", corner_radius=15, text="ホーム", width=215, height=40, font=("BIZ UDゴシック", 16, "normal"), anchor="w")
+  button_home.bind('<Enter>', on_enter_home, add='+')
+  button_home.bind("<Leave>", on_leave_home, add='+')
+  button_home.place(x=5,y=12)
+  
+  librarylist = ctk.CTkFrame(master=root, width=230, height=384, corner_radius=0, fg_color="#688fbf")
+  librarylist.place(x=0,y=384)
+  
+  tk.Label(librarylist, bg="#688fbf", text="ライブラリー", fg="#fff", font=("BIZ UDゴシック", 13, "normal")).place(x=15,y=0)
+  
+  button_library = ctk.CTkButton(master=librarylist, command=lambda: setup_cotent_sidebar(1, 2, 1), image=ctk.CTkImage(Image.open("data/icon_library_disable.png"),size=(20, 20)), compound="left", fg_color="#0f1314", hover_color="#2b373a", corner_radius=15, text="ダウンロードリスト", width=215, height=40, font=("BIZ UDゴシック", 14, "normal"), anchor="w")
+  button_library.bind('<Enter>', on_enter_library, add='+')
+  button_library.bind("<Leave>", on_leave_library, add='+')
+  button_library.place(x=5,y=24)
+  
+  button_library_already = ctk.CTkButton(master=librarylist, command=lambda: setup_cotent_sidebar(1, 2, 2), image=ctk.CTkImage(Image.open("data/icon_library_disable.png"),size=(20, 20)), compound="left", fg_color="#0f1314", hover_color="#2b373a", corner_radius=15, text="ダウンロード済みリスト", width=215, height=40, font=("BIZ UDゴシック", 14, "normal"), anchor="w")
+  button_library_already.bind('<Enter>', on_enter_library_already, add='+')
+  button_library_already.bind("<Leave>", on_leave_library_already, add='+')
+  button_library_already.place(x=5,y=69)
   #tk.Canvas(bg="#0a111e", highlightthickness=0, height=2080, width=4).place(x=230, y=0)
   #ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/join_leave.png"),size=(20, 20)), compound="left", fg_color="#0f1314", hover_color="#ade3f7", corner_radius=0, text="Joiner / Leaver", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w", command= lambda: module_scroll_frame(1, 1)).place(x=20,y=12)
   #ctk.CTkButton(master=modulelist, image=ctk.CTkImage(Image.open("data/spammer.png"),size=(20, 20)), compound="left", fg_color="#0f1314", hover_color="#ade3f7", corner_radius=0, text="Spammer", width=195, height=40, font=("Roboto", 16, "bold"), anchor="w", command= lambda: module_scroll_frame(1, 2)).place(x=20,y=57)
@@ -72,7 +138,9 @@ def setup_content():
   print("coming soon lol")
 
 printl("info", "Loading GUI")
+
 setup_sidebar()
+setup_cotent_sidebar(2, 1)
 setup_content()
 
 
